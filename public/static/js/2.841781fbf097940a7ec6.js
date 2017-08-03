61942630afc6f1cb6204535cc739659d14621803
amg <ambitious.gyang@gmail.com>
2017-08-03 18:05:51 +0800

    添加cdn配置，测试lodash

---
+++ public/static/js/2.841781fbf097940a7ec6.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{85:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(88),s=n(89),r=n(7),u=r(a.a,s.a,null,null,null);t.default=u.exports},88:function(e,t,n){"use strict";t.a={name:"asyncTest",mounted:function(){alert("inited!")}}},89:function(e,t,n){"use strict";var a=function(){var e=this,t=e.$createElement;return(e._self._c||t)("div",{staticClass:"alert"},[e._v("234")])},s=[],r={render:a,staticRenderFns:s};t.a=r}});
+//# sourceMappingURL=2.841781fbf097940a7ec6.js.map
