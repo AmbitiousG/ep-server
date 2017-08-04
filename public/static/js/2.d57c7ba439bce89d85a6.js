ed0f9f3a4a47e9aab80c693633ec88e4021989b5
amg <ambitious.gyang@gmail.com>
2017-08-04 14:02:38 +0800

    修复外部引入版本导致的vue-router不起作用的问题

---
+++ public/static/js/2.d57c7ba439bce89d85a6.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{21:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(24),s=n(25),r=n(0),u=r(a.a,s.a,null,null,null);t.default=u.exports},24:function(e,t,n){"use strict";t.a={name:"asyncTest",mounted:function(){alert("inited!")}}},25:function(e,t,n){"use strict";var a=function(){var e=this,t=e.$createElement;return(e._self._c||t)("div",{staticClass:"alert"},[e._v("234")])},s=[],r={render:a,staticRenderFns:s};t.a=r}});
+//# sourceMappingURL=2.d57c7ba439bce89d85a6.js.map
