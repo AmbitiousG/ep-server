60dd39c3548b84fcd38018a358e24f47c1ad48b7
GY-PC\GY <ambitious.gyang@gmail.com>
2017-08-07 23:00:56 +0800

    添加 saveItem

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var getList = require('./routes/api/getList');
+var saveItem = require('./routes/api/saveItem');
 
 var history = require('connect-history-api-fallback');
 
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/api', getList);
+app.use('/api', saveItem);
 
 // app.post('/api/getList', (req, res) => {
 //   res.send('api/getList')
