a466fc189360a6f6d7794d01df79fbe4d34eb5ef
GY-PC\GY <ambitious.gyang@gmail.com>
2017-08-08 22:04:18 +0800

    使用vux重构，适配mobile

--- public/static/js/3.c78c9c3efc78a55c2fc7.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{22:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(26),s=n(27),r=n(0),u=r(a.a,s.a,null,null,null);t.default=u.exports},26:function(e,t,n){"use strict";t.a={name:"asyncTest",mounted:function(){alert("inited!")}}},27:function(e,t,n){"use strict";var a=function(){var e=this,t=e.$createElement;return(e._self._c||t)("div",{staticClass:"alert"},[e._v("234")])},s=[],r={render:a,staticRenderFns:s};t.a=r}});
-//# sourceMappingURL=3.c78c9c3efc78a55c2fc7.js.map
