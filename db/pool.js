0a52c9b464a71764d6b91f53ab6429ef3599e379
amg <ambitious.gyang@gmail.com>
2017-08-11 13:37:41 +0800

    port 配置

--- db/pool.js
+++ db/pool.js
@@ -1,15 +1,17 @@
 var _ = require('lodash');
 var mysql = require('mysql');
 var config = require('./config');
+var { host, user, password, database } = config;
+
 var pool = mysql.createPool(_.assign({
   connectionLimit: 10,
   host: '',
   user: '',
   password: '',
   database: ''
-}, config));
+}, { host, user, password, database }));
 
-pool.on('connection', function (connection) {
+pool.on('connection', function(connection) {
   console.log('connected');
 });
 
