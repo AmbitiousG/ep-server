dff4085cd6068b22670f498f2e548a783a376491
amg <ambitious.gyang@gmail.com>
2017-08-15 09:30:55 +0800

    控制金额输入type fix(only numbers)

--- public/static/js/3.d54bcdc19b886fe1cf22.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{188:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(203),s=n(204),r=n(0),u=r(a.a,s.a,null,null,null);t.default=u.exports},203:function(e,t,n){"use strict";t.a={name:"asyncTest",mounted:function(){alert("inited!")}}},204:function(e,t,n){"use strict";var a=function(){var e=this,t=e.$createElement;return(e._self._c||t)("div",{staticClass:"alert"},[e._v("234")])},s=[],r={render:a,staticRenderFns:s};t.a=r}});
-//# sourceMappingURL=3.d54bcdc19b886fe1cf22.js.map
