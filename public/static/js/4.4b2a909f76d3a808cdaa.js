75c44ad31d1d7fbbf1dafbd0a85a72454973b6c3
GY-PC\GY <ambitious.gyang@gmail.com>
2017-08-15 21:59:34 +0800

    添加登录提示，添加登出按钮

--- public/static/js/4.4b2a909f76d3a808cdaa.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([4],{188:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(228),s=n(229),r=n(0),u=r(a.a,s.a,null,null,null);t.default=u.exports},228:function(e,t,n){"use strict";t.a={name:"asyncTest",mounted:function(){alert("inited!")}}},229:function(e,t,n){"use strict";var a=function(){var e=this,t=e.$createElement;return(e._self._c||t)("div",{staticClass:"alert"},[e._v("234")])},s=[],r={render:a,staticRenderFns:s};t.a=r}});
-//# sourceMappingURL=4.4b2a909f76d3a808cdaa.js.map
